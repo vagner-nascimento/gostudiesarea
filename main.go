807e41e1d6e441c895e4c2e136d0ef74b046f607
Vagner Nascimento <vagnernasc88@gmail.com>
2019-12-30 22:39:38 -0300

    Create main.go

---
+++ main.go
@@ -0,0 +1,20 @@
+package gostudiesarea
+
+import "math"
+
+// Pi is a known math const
+const Pi = 3.1416
+
+// Circ calculates the circumference of a circle
+func Circ(rain float64) float64 {
+	return math.Pow(rain, 2) * Pi
+}
+
+// Rect calculate the area of a rectangle
+func Rect(base, height float64) float64 {
+	return base * height
+}
+
+func _EqTriangle(base, height float64) float64 { // This function is not accessible
+	return (base * height) / 2
+}
